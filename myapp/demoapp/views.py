be859a58ef244a76b7a9b74472104107adf1239b
Nilesh Sharma <53362304+nileshsharma-cloud@users.noreply.github.com>
2020-06-21 17:34:03 +0530

    Created framework for api using python

---
+++ myapp/demoapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.views import APIView
+from .serializers import employeeSerializer
+from .models import Employees
+from rest_framework.response import Response
+# Create your views here.
+
+class EmployeeViewSet(APIView):
+
+    def get(self, request):
+        employees1 = Employees.objects.all()
+        serializer = employeeSerializer(employees1,many=True)
+        return Response(serializer.data)
+
+    def post(self):
+        pass
+
